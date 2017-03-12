bf62edd7004c4bbe99835539df8c57682dcf2614
Sebastien Van Cauwenberghe <svancau@gmail.com>
2017-03-12 19:43:32 +0100

    Simplify packet framing logic
    Send bytes to decoder when a 0 is received, supposedly
    END OF FRAME, do not care about begin of frame

--- framer.py
+++ framer.py
@@ -11,25 +11,20 @@ class Framer():
         self.fieldLength = 0
         self.packet = bytearray()
         self.packetFifo = deque()
-        self.capture = False
         self.depack = ReceivePacket()
 
     def clearBuffers(self):
         self.packet = bytearray()
 
     def processInput(self, data):
-        if self.capture:
-            if data != b'\x00':
-                self.packet += data
-            else:
-                extractedPacket = self.depack.unpackData(self.packet)
-                if extractedPacket != None:
-                    #print('Packet OK '+str(extractedPacket))
-                    self.packetFifo.append(extractedPacket)
-                self.packet = bytearray()
+        if data != b'\x00':
+            self.packet += data
         else:
-            if data == b'\x00':
-                self.capture = True
+            extractedPacket = self.depack.unpackData(self.packet)
+            if extractedPacket != None:
+                #print('Packet OK '+str(extractedPacket))
+                self.packetFifo.append(extractedPacket)
+            self.packet = bytearray()
 
     def incomingPackets(self):
         if len(self.packetFifo) > 0:
